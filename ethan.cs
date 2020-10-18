e938a715312d999708b695b94c11bac2b6d5cf71
Christian Torres Gonzalez <56066682+ChristianTorresGonzalez@users.noreply.github.com>
2020-10-18 19:17:44 +0100

    Añadiendo scripts de los objetos

---
+++ ethan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ethan : MonoBehaviour
+{
+    public string nombre;
+    public int identificador = 6;
+    public int contador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nombre = gameObject.name;
+        contador = 0;
+        Debug.Log(nombre + " - Identificador numerico: " + identificador);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Contador " + nombre + ": " + contador);
+        contador++;
+    }
+}
